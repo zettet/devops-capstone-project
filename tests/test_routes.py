2df389102c2ed1438c90f48ae325f8a4ec1a5843
zettet <tzettersten@gmail.com>
2023-08-21 17:58:22 -0400

    adding security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,6 +9,7 @@ import os
 import logging
 from unittest import TestCase
 from tests.factories import AccountFactory
+from service import talisman
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
@@ -19,7 +20,7 @@ DATABASE_URI = os.getenv(
 
 ACCOUNT_BASE_URL = "/account"
 ACCOUNTS_BASE_URL = "/accounts"
-
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 ######################################################################
 #  T E S T   C A S E S
@@ -35,6 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -224,6 +226,19 @@ class TestAccountService(TestCase):
             f"{ACCOUNT_BASE_URL}/0",
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)    
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
     def assert_account(self, actual_account, expected_account):
         self.assertEqual(actual_account["name"], expected_account.name)
